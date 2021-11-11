254b14fc60f6783f6e8e7f4f50a225b4c70c2ea2
Sanshit Sagar <sasagar@ucsd.edu>
2021-11-11 08:06:44 -0800

    adding basic styles to index.html

---
+++ .history/server_20211111055748.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
+
+
+const app = express(); 
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+}); 
+
+const socketServer = new WebSocket.Server({ port: 3030 }); 
+
+socketServer.on('connection', (socketClient) => {
+    console.log('connected');
+    console.log('client Set Length: ' + socketClient.clients.size);
+
+    socketClient.on('close', (socketClient) => {
+        console.log('closed')
+    })
+})
---
+++ .history/server_20211111060410.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
+
+
+const app = express(); 
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+}); 
+
+const socketServer = new WebSocket.Server({
+    port: 3030 
+}); 
+
+const messages = ['Welcome to the WebSockets Demo'];
+
+socketServer.on('connection', (socketClient) => {
+    console.log('connected');
+    console.log('client Set Length: ' + socketServer.clients.size);
+    socketClient.send(JSON.stringify(messages)); 
+
+    socketClient.on('close', (socketClient) => {
+        console.log('closed');
+        console.log('Number of clients: ' + socketServer.clients.size); 
+    })
+})
